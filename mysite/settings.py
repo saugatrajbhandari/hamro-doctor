3b06b39cdcb1fd3870ac309722144e28160cd44d
saugat <saugatshrestha602@gmail.com>
2022-06-13 11:26:00 +0545

    m

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-*^rb4ql*nn)jxqq0l6^&iy^lyr^tk@iykxa1vcck25@p!j34j&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'harmodoctor.herokuapp.com']
 
 
 # Application definition
